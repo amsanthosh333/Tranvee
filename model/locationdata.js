cc5217e3e5fad8661df97c41b2cc938908db0bb2
manojkumarr2121 <manoj@neophrontech.com>
2020-07-21 01:09:25 -0700

    make it better

--- model/locationdata.js
+++ model/locationdata.js
@@ -3,6 +3,33 @@ const mongoose = require("mongoose");
 const locationdataSchema = new mongoose.Schema({
     url: {
         type: String,
+    }, 
+    deviceid: {
+        type: String,
+    },
+    time: {
+        type: String,
+    },
+    lat: {
+        type: String,
+    },
+    lon: {
+        type: String,
+    },
+    speed: {
+        type: String,
+    },
+    couse: {
+        type: String,
+    },
+    accuracy: {
+        type: String,
+    },
+    battery: {
+        type: String,
+    },
+    mock: {
+        type: String,
     },
     datetime: {
         type: String,
