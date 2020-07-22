72af193aa03febb36778564e4b6ce9f75dff2885
manojkumarr2121 <manoj@neophrontech.com>
2020-07-22 02:10:25 -0700

    make it better

--- router/user.js
+++ router/user.js
@@ -4,13 +4,13 @@ const userController = require('./../controller/user');
 
 
 router.post('/register', async (req, res) => {
-    res.send(await userController.register(req.body.username, req.body.password,req.body.email,req.body.phone));
+    res.send(await userController.register(req.body.username,req.body.password,req.body.email,req.body.phone));
 });
 
 router.post('/login', async (req, res) => {
     res.send(await userController.login(req.query.username, req.query.password));
 });
 
-module.exports = router;
+
 
 module.exports = router;
