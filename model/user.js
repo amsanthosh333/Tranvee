de4833feb28bb3ccbfd57778c56297c2d4a39d62
manojkumarr2121 <manoj@neophrontech.com>
2020-07-22 00:18:22 -0700

    make it better

--- model/user.js
+++ model/user.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    city: {
+    password: {
         type: String,
         required: true,
     }
