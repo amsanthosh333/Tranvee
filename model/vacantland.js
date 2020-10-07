a903a807c47dd3e404b425696586f68e9291207b
santhiya <santhiya@neophrontech.com>
2020-10-07 13:38:27 +0530

    make it better

--- model/vacantland.js
+++ model/vacantland.js
@@ -4,6 +4,43 @@ const vancantlandSchema = new mongoose.Schema({
     Date: {
         type: String,
         required: true
+    },
+    Area: {
+        type: String,
+        required: true
+    },
+    totalSizesq: {
+        type: String,
+        required: true
+    },
+    Northsizesq: {
+        type: String,
+        required: true
+    },
+    Eastsizesq: {
+        type: String,
+        required: true
+    },
+    Westsizesq: {
+        type: String,
+        required: true
+    },
+    Southsizesq: {
+        type: String,
+        required: true
+    },
+    RateperSq: {
+        type: String,
+        required: true
+    },
+    DTCP: {
+        type: String,
+        required: true
+    },
+    Unapproved: {
+        type: String,
+        required: true
     }
+
 })
 module.exports = new mongoose.model('vancant',vancantlandSchema);
