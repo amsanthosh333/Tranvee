cff1a02c20120f6bdcc82b158459d9f4c8708e51
santhiya <santhiya@neophrontech.com>
2020-10-07 13:54:44 +0530

    make it better

--- model/vacantland.js
+++ model/vacantland.js
@@ -40,6 +40,106 @@ const vancantlandSchema = new mongoose.Schema({
     Unapproved: {
         type: String,
         required: true
+    },
+    FrontRoadwidth: {
+        type: String,
+        required: true
+    },
+    CornerSite: {
+        type: String,
+        required: true
+    },
+    Corporationwater: {
+        type: String,
+        required: true
+    },
+    LTWater: {
+        type: String,
+        required: true
+    },
+    BorewellLevel: {
+        type: String,
+        required: true
+    },
+    Drainage: {
+        type: String,
+        required: true
+    },
+    Undergrounddrainage: {
+        type: String,
+        required: true
+    },
+    EBPost: {
+        type: String,
+        required: true
+    },
+    GuidelineValue: {
+        type: String,
+        required: true
+    },
+    Budget: {
+        type: String,
+        required: true
+    },
+    MAP: {
+        type: String,
+        required: true
+    },
+    MAPIMAGE: {
+        type: String,
+        required: true
+    },
+    EC: {
+        type: String,
+        required: true
+    },
+    MainRoadFacing: {
+        type: String,
+        required: true
+    },
+    Distance: {
+        type: String,
+        required: true
+    },
+    EBLineCrossing: {
+        type: String,
+        required: true
+    },
+    Vacantlandtax: {
+        type: String,
+        required: true
+    },
+    RegisterOffice: {
+        type: String,
+        required: true
+    },
+    OwnersDetailsName: {
+        type: String,
+        required: true
+    },
+    OwnersDetailsMobile: {
+        type: String,
+        required: true
+    },
+    Others: {
+        type: String,
+        required: true
+    },
+    Refertype: {
+        type: String,
+        required: true
+    },
+    ReferName: {
+        type: String,
+        required: false
+    },
+    ReferContact: {
+        type: String,
+        required: false
+    },
+    Sold: {
+        type: String,
+        required: true
     }
 
 })
