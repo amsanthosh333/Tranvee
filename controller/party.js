63e51b18b6e89abf977d2547b8bb847e65955067
santhiya <santhiya@neophrontech.com>
2020-11-17 13:45:19 +0530

    make it better

---
+++ controller/party.js
@@ -0,0 +1,74 @@
+const partySchema = require('./../model/party');
+const errorHandler = require('../utils/error.handler');
+
+class partyController{
+
+
+	async add(farm){
+		try{
+			let response = await partySchema.create(farm);
+			return { status: "success",   msg:"farm Added successfully", result: response, message: "Added Successfully" };
+		} catch(error){
+			return {
+				status: "error",
+				error: errorHandler.parseMongoError(error)
+			};
+		}
+	}
+	
+	async fetch(){
+		try{
+			let response = await partySchema.find({});
+			return {
+				response: response
+			};
+		} catch(error){
+			return {
+				status: "error",
+				error: errorHandler.parseMongoError(error)
+			};
+		}
+	}
+
+	async fetchdata(id){
+		try{
+			let response = await partySchema.find({'_id':id});
+			return response;
+			
+		} catch(error){
+			return {
+				status: "error",
+				error: errorHandler.parseMongoError(error)
+			};
+		}
+	}
+
+	async delete(id){
+		try{
+			let response = await partySchema.deleteOne({_id: id});
+			return {
+				status: "success",
+				response: response
+			};
+		} catch(error){
+			return {
+				status: "error",
+				error: errorHandler.parseMongoError(error)
+			};
+		}
+	}
+
+	async update(id, body) {
+
+        try {
+            let response = await partySchema.update({_id: id}, body);
+            return { status: "success", msg:"Vancantland Updated successfully",result: response, message: "Updated Successfully" };
+
+        } catch (error) {
+            return { status: "error", error: error };
+        }
+
+    }
+
+}
+module.exports = new partyController();
