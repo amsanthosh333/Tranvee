56c1b54ad1dd9ab76f0af005dd46381795d28a35
santhiya <santhiya@neophrontech.com>
2021-02-09 12:35:45 +0530

    make it better

--- model/admin.js
+++ model/admin.js
@@ -9,6 +9,14 @@ const adminSchema = new mongoose.Schema({
     phone: {
         type: String,
         required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
     }
 });
 
