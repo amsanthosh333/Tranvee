6fd4a0ce7e33a0154eebcc1595793ace40ac9223
santhiya <santhiya@neophrontech.com>
2021-03-05 11:59:21 +0530

    make it better

--- controller/customer.js
+++ controller/customer.js
@@ -75,6 +75,77 @@ class CustomerController {
             }
         }
     }
+
+
+	async add(farm){
+		try{
+			let response = await customerSchema.create(farm);
+			return { status: "success",   msg:"Customer Added successfully", result: response, message: "Added Successfully" };
+		} catch(error){
+			return {
+				status: "error",
+				error: errorHandler.parseMongoError(error)
+			};
+		}
+	}
+	
+	async fetch(){
+		try{
+			let response = await customerSchema.find({});
+			let count=Object.keys(response).length;
+			return {
+				response: response,
+				count
+			};
+		} catch(error){
+			return {
+				status: "error",
+				error: errorHandler.parseMongoError(error)
+			};
+		}
+	}
+
+	async fetchdata(id){
+		try{
+			let response = await customerSchema.find({'_id':id});
+			return response;
+			
+		} catch(error){
+			return {
+				status: "error",
+				error: errorHandler.parseMongoError(error)
+			};
+		}
+	}
+
+	async delete(id){
+		try{
+			let response = await customerSchema.deleteOne({_id: id});
+			return {
+				status: "success",
+				response: response
+			};
+		} catch(error){
+			return {
+				status: "error",
+				error: errorHandler.parseMongoError(error)
+			};
+		}
+	}
+
+	async update(id, body) {
+
+        try {
+            let response = await customerSchema.update({_id: id}, body);
+            return { status: "success", msg:"Customer Updated successfully",result: response, message: "Updated Successfully" };
+
+        } catch (error) {
+            return { status: "error", error: error };
+        }
+
+    }
+
+
 }
 
        
--- router/customer.js
+++ router/customer.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const customerController = require('../controller/customer');
 
-
-
 router.post('/register', async (req, res) => {
     res.send(await customerController.register(req.body));
 });
@@ -14,5 +12,26 @@ router.post('/login', async (req, res) => {
 router.get('/login1', async (req, res) => {
     res.send(await customerController.login1(req.query.username,req.query.password));
 });
-
+router.post('/add', async (req, res) => {
+	const response = await customerController.add(req.body);
+	res.send(response);
+})
+router.get('/', async (req, res) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	const response = await customerController.fetch();
+	res.send(response);
+})
+router.get('/fetchdata', async (req, res) => {
+	//res.setHeader('Access-Control-Allow-Origin', '*');
+	const response = await customerController.fetchdata(req.query.id);
+	res.send(response);
+})
+router.delete('/delete', async (req, res) => {
+	const response = await customerController.delete(req.query.id);
+	res.send(response);
+})
+router.put('/update', async (req, res) => {
+	const response = await customerController.update(req.query.id, req.body);
+	res.send(response);
+})
 module.exports = router;
