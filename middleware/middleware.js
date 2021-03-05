2b5b4b8ce622dcaefeb094a5df57038c11e3062a
santhiya <santhiya@neophrontech.com>
2021-03-05 16:02:04 +0530

    make it better

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -37,7 +37,7 @@ mongoose.connect(db, {
 	//locationdata
 
 server.use("/user", userRouter);
-// server.use("/customer", customerRouter);
+server.use("/customer", customerRouter);
 server.use("/customer", async (req, res, next) => {
     console.log("eeeeeeee",""+req.headers.authorization);
 	if(!req.headers.authorization){
