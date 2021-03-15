920fa11d4c2d22c0b16fd8506dbae75ef48c463b
santhiya <santhiya@neophrontech.com>
2021-03-15 13:52:24 +0530

    make it better

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -45,16 +45,7 @@ mongoose.connect(db, {
 server.use("/user", userRouter);
 // server.use("/customer", customerRouter);
 server.use("/customer", async (req, res, next) => {
-    console.log("eeeeeeee",""+req.headers.authorization);
-	if(!req.headers.authorization){
-		return res.send({
-			status: 'error',
-			msg: 'Invalid Token'
-		})
-	}
-
-	await customerController.validateToken(res,req.headers.authorization);
-
+   
 	next();
 },customerRouter);
 server.use("/vechicle", async (req, res, next) => {
