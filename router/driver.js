859e01e923dac565b1ded7527284da0fb3dda4f6
santhiya <santhiya@neophrontech.com>
2021-03-20 15:37:38 +0530

    make it better

--- router/driver.js
+++ router/driver.js
@@ -16,24 +16,24 @@ router.get('/login1', async (req, res) => {
 router.post('/add', async (req, res) => {
 	const response = await driverController.add(req.body);
 	res.send(response);
-})
+});
 router.get('/', async (req, res) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	const response = await driverController.fetch();
 	res.send(response);
-})
+});
 router.get('/fetchdata', async (req, res) => {
 	//res.setHeader('Access-Control-Allow-Origin', '*');
 	const response = await driverController.fetchdata(req.query.id);
 	res.send(response);
-})
+});
 router.delete('/delete', async (req, res) => {
 	const response = await driverController.delete(req.query.id);
 	res.send(response);
-})
+});
 router.put('/update', async (req, res) => {
 	const response = await driverController.update(req.query.id, req.body);
 	res.send(response);
-})
+});
 
 module.exports = router;
