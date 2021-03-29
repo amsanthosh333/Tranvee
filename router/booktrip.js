1b4f5062fc23b76f28ec71289ba74a9bbd497268
santhiya <santhiya@neophrontech.com>
2021-03-29 16:43:12 +0530

    make it better

--- router/booktrip.js
+++ router/booktrip.js
@@ -44,27 +44,50 @@ router.put('/update', async (req, res) => {
 	res.send(response);
 })
 router.put('/amount_update', async (req, res) => {
-	let detailswaitingcalculation = await booktripSchema.find({'_id':req.query.id});
+
+
+	// console.log("minutes",req.query._id);
+
+
+	let detailswaitingcalculation = await booktripSchema.find({'_id':req.query._id});
 
 	let vechicalid=detailswaitingcalculation[0].vechical;
 	let StartotpTime=detailswaitingcalculation[0].StartotpTime;
 	let StartTripTime=detailswaitingcalculation[0].StartTripTime;
 	let Endtriptime=detailswaitingcalculation[0].Endtriptime;
 	let ReachDestinationTime=detailswaitingcalculation[0].ReachDestinationTime;
-	let estimate=detailswaitingcalculation[0].Amount;
+	let estimate=parseInt(detailswaitingcalculation[0].Amount);
 
-	   let totalDiff = StartTripTime-StartotpTime;
-         hours = Math.floor((totalDiff/60));
-         minutes =(totalDiff%60);
-
-		 console.log("minutes",""+minutes);
-         
-	let vechicleSchemacalculation = await vechicleSchema.find({'_id':vechicalid});
+	     const diffInMilliseconds = Math.abs(new Date(StartotpTime) - new Date(StartTripTime))/1000;
+	     const firstminminutes = Math.floor(diffInMilliseconds / 60) % 60;
+		 console.log("minutes",firstminminutes);
+		 const diffInMilliseconds1 = Math.abs(new Date(Endtriptime) - new Date(ReachDestinationTime))/1000;
+	     const secondminminutes = Math.floor(diffInMilliseconds1 / 60) % 60;
+	     console.log("minutes",secondminminutes);
+         let totalmin=firstminminutes+secondminminutes;
+		 console.log("totalminutes",totalmin);
+	   let vechicleSchemacalculation = await vechicleSchema.find({'_id':vechicalid});
 
 	let Waiting_min=vechicleSchemacalculation[0].Waiting_min;
 	let min_waiting_time=vechicleSchemacalculation[0].min_waiting_time;
+
+	let calculate=totalmin-Waiting_min;
+
+	console.log("calculate",calculate);
+	const realamount=calculate*min_waiting_time;
+
+	const sum = estimate + realamount ;
+
+	console.log("realamount",sum);
+
+	let member={
+		"Endtriptime":req.body.Endtriptime,
+		"Bookingstatus":"Closed",
+		"Amount": sum,
+		 }
+
 	
-	const response = await booktripController.amount_update(req.query.id, req.body);
+	const response = await booktripController.amount_update(req.query.id, member);
 	res.send(response);
 })
 router.get('/aggregation', async (req, res) =>{
