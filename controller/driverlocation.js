01a581dbfac751d61ab1140b256145402e6958c9
Manojkumarr130308012 <31697317+Manojkumarr130308012@users.noreply.github.com>
2021-08-11 16:52:32 +0530

    make it better

--- controller/driverlocation.js
+++ controller/driverlocation.js
@@ -10,30 +10,11 @@ class driverlocationController{
 			let driverid=farm.Driverid;
          console.log("driverid",""+driverid);
 
+		 var dateTime = require('node-datetime');
+		 var dt = dateTime.create();
+		 farm.lastupdate= dt.format('Y-m-d H:M:S');
 
-		 let date_ob = new Date();
-
-		 // current date
-		 // adjust 0 before single digit date
-		 let date = ("0" + date_ob.getDate()).slice(-2);
-		 
-		 // current month
-		 let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-		 
-		 // current year
-		 let year = date_ob.getFullYear();
-		 
-		 // current hours
-		 let hours = date_ob.getHours();
-		 
-		 // current minutes
-		 let minutes = date_ob.getMinutes();
-		 
-		 // current seconds
-		 let seconds = date_ob.getSeconds();
-
-        farm.lastupdate=date_ob.toTimeString();
-	     	 
+        
            let user = await driverlocationSchema.findOne({
            	Driverid: driverid
            });
