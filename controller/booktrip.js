5500bbb05fb916f0a3de1930d388adfc738e377c
Manojkumarr130308012 <31697317+Manojkumarr130308012@users.noreply.github.com>
2021-08-27 15:41:47 +0530

    make it better

--- controller/booktrip.js
+++ controller/booktrip.js
@@ -309,10 +309,14 @@ class booktripController{
 		let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
 		
 		let year = date_ob.getFullYear();
-		
+		let hours = date_ob.getHours();
+
+		let minutes = date_ob.getMinutes();
+
+		let seconds = date_ob.getSecond
 		   
 		
-		let date = year + "-" + month + "-" + day;
+		let date = year + "-" + month + "-" + day + " " + hours + ":" + minutes + ":" + seconds;
 		
 		console.log(date);
 		
@@ -436,7 +440,7 @@ class booktripController{
 		body.Closedate=date;
 
 		console.log('messagestatus:',body);
-		
+
         try {
 			const message = { 
 				notification: {
