a587183f817d633638e4a3e4a2d7bd9a96d098b7
ManojKumar <manoj@astramint.com>
2022-03-26 12:28:14 +0530

    make it better

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -27,7 +27,7 @@ const planRouter = require('./../router/plan');
 
 // console.log("enter")
  let { protocal, host, port, name,username,password } = config.app.db;
-//  let db= process.env.MONGODB_URL ||`mongodb+srv://admin:admin123@cluster0.qcrci.mongodb.net/schoolsms?retryWrites=true&w=majority`;
+//  let db= process.env.MONGODB_URL ||`mongodb+srv://admin:admin123@cluster0.qcrci.mongodb.net/schoolsms?retryWrites=true&w=majority`;/
  let db= process.env.MONGODB_URL ||`mongodb+srv://admin:admin1234@tranvee.dg9oa.mongodb.net/Project0?retryWrites=true&w=majority`;
 
 console.log('connected to the database',db);
