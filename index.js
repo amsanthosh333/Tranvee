adc726b1dc24c512fee30938668ba1b332beca65
ManojKumar <manoj@astramint.com>
2022-07-06 15:55:07 +0530

    make it better

--- index.js
+++ index.js
@@ -10,5 +10,9 @@ const config=require('./config/config.json');
 const middleware=require('./middleware/middleware')
 app.use(middleware);
 
+app.get('/', (req, res) => {
+    console.log(req.query);
+    res.sendStatus(200);
+});
 
 app.listen(port, () => console.log(`url-shortener listening on port ${config.app.port}!`));
